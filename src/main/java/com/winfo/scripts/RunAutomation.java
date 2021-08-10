c6e001b62fa45d66a54220175f6b5b290c8cc378
chakradhar <chakradhar.mandapati@winfosolutions.com>
2021-08-10 16:52:29 +0530

    jira changes

--- src/main/java/com/winfo/scripts/RunAutomation.java
+++ src/main/java/com/winfo/scripts/RunAutomation.java
@@ -727,7 +727,8 @@ public class RunAutomation {
 								seq_num + "_" + script_Number + ".pdf", startdate, enddate);
 						limitScriptExecutionService.insertTestRunScriptData(fetchConfigVO, fetchMetadataListVO,
 								script_id1, script_Number, "pass", startdate, enddate);
-
+						limitScriptExecutionService.updateFaileScriptscount(test_set_line_id,
+								test_set_id);
 //						uploadPDF(fetchMetadataListVO, fetchConfigVO);
 					}
 					System.out.println("Successfully Executed the" + "" + actionName);
