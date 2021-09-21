f6475705715889dd4c398c50c49a3e4509a838c8
Ganguraju-winfo <ganguraju.gundeboina@winfosolutions.com>
2021-09-21 12:55:04 +0530

    xpathDBlogic without Xpathperformance code

--- src/main/java/com/winfo/config/DriverConfiguration.java
+++ src/main/java/com/winfo/config/DriverConfiguration.java
@@ -45,8 +45,8 @@ public class DriverConfiguration {
 			DesiredCapabilities cap = DesiredCapabilities.chrome();
 			if (os.contains("win")) {
 				System.out.println("windows location");
-//				options.setBinary("C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe");// cap.setCapability("chrome.binary",
-				options.setBinary("C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe");														// "C:\\Program
+				options.setBinary("C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe");// cap.setCapability("chrome.binary",
+																										// "C:\\Program
 																										// Files
 																										// (x86)\\Google\\Chrome\\Application\\chrome.exe");
 			} else {
@@ -66,8 +66,8 @@ public class DriverConfiguration {
 			cap.setCapability(CapabilityType.ACCEPT_SSL_CERTS, true);
 			cap.setCapability(ChromeOptions.CAPABILITY, options);
 
-			 driver = new ChromeDriver(cap);
-//			driver = new RemoteWebDriver(new URL(config_url), cap);
+//			 driver = new ChromeDriver(cap);
+			driver = new RemoteWebDriver(new URL(config_url), cap);
 //			http://watsudgs01.winfosolutions.com:4444/wd/hub
 
 		} else if (BrowserConstants.FIREFOX.value.equalsIgnoreCase(fetchConfigVO.getBrowser())) {
