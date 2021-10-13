c82f4c2e554e746316542f0d56f8535e794e1faa
Ganguraju-winfo <ganguraju.gundeboina@winfosolutions.com>
2021-10-13 09:06:16 +0530

    new xpathPerformance DB changes

--- src/main/java/com/winfo/services/ScriptXpathService.java
+++ src/main/java/com/winfo/services/ScriptXpathService.java
@@ -16,52 +16,18 @@ public class ScriptXpathService {
 	WatsXpathDao dao;
 	
 	@Transactional
-	public void saveXpathParams(String param1,String param2,String scripNumber,String xpath, String action, String lineNumber) {
-		if(param2.equals("")|| param2==null) {
-//			String xpathlocation=dao.checkXpathlocation(param1,scripNumber);
-//			if(xpathlocation==null) {
-			
-				dao.saveXpathParams(param1,scripNumber,xpath,action,lineNumber);
-//			}
-		}
-		else {
-			String params=param1+">"+param2;
-//			String xpathlocation=dao.checkXpathlocation(params,scripNumber);
-//			if(xpathlocation==null) {
-	
-				int i=dao.saveXpathParams(params,scripNumber,xpath,action,lineNumber);
-////				if(i==0) {
-//					String params1="(*)"+param1+">"+param2;
-//					dao.saveXpathParams(params1,scripNumber,xpath);
-////				}
-//			}
-			
-		}
-	}
-
-	@Transactional
-	public String getXpathParams(String param1, String param2, String scriptID, String action) {
-		String xpathlocation =null;
-		if(param2.equals("")|| param2==null) {
-			 xpathlocation=dao.checkXpathlocation1(param1,scriptID,action);
-		
-		}
-		else {
-			String params=param1+">"+param2;
-			 xpathlocation=dao.checkXpathlocation1(params,scriptID,action);
-	
-			
-		}
-
-		return xpathlocation;
-		
-		
+	public void saveXpathParams(String scriptID, String metadataID, String xpath) {
+		dao.saveXpathParams(scriptID,metadataID,xpath);
 	}
 	@Transactional
-	public String checkValidScript(String scripNumber) {
+	public String getXpathParams(String scriptID, String metadataID) {
+		return dao.getXpathParams(scriptID,metadataID);
+	}
+	@Transactional
+	public String checkValidScript(String scriptID) {
 		String status=null;
-		Timestamp executionDate=dao.executionDate(scripNumber);
-		Timestamp scriptUpdateDate=dao.scriptUpdateDate(scripNumber);
+		Timestamp executionDate=dao.executionDate(scriptID);
+		Timestamp scriptUpdateDate=dao.scriptUpdateDate(scriptID);
 		if(executionDate!=null&&scriptUpdateDate!=null) {
 		  long l1 = executionDate.getTime();
 		    long l2 = scriptUpdateDate.getTime();
@@ -80,6 +46,52 @@ public class ScriptXpathService {
 
 		return status;
 	}
+//	@Transactional
+//	public void saveXpathParams(String param1,String param2,String scripNumber,String xpath, String action, String lineNumber) {
+//		if(param2.equals("")|| param2==null) {
+////			String xpathlocation=dao.checkXpathlocation(param1,scripNumber);
+////			if(xpathlocation==null) {
+//			
+//				dao.saveXpathParams(param1,scripNumber,xpath,action,lineNumber);
+////			}
+//		}
+//		else {
+//			String params=param1+">"+param2;
+////			String xpathlocation=dao.checkXpathlocation(params,scripNumber);
+////			if(xpathlocation==null) {
+//	
+//				int i=dao.saveXpathParams(params,scripNumber,xpath,action,lineNumber);
+//////				if(i==0) {
+////					String params1="(*)"+param1+">"+param2;
+////					dao.saveXpathParams(params1,scripNumber,xpath);
+//////				}
+////			}
+//			
+//		}
+//	}
+
+	
+//	@Transactional
+//	public String getXpathParams(String param1, String param2, String scriptID, String action) {
+//		String xpathlocation =null;
+//		if(param2.equals("")|| param2==null) {
+//			 xpathlocation=dao.checkXpathlocation1(param1,scriptID,action);
+//		
+//		}
+//		else {
+//			String params=param1+">"+param2;
+//			 xpathlocation=dao.checkXpathlocation1(params,scriptID,action);
+//	
+//			
+//		}
+//
+//		return xpathlocation;
+//		
+//		
+//	}
+
+
+	
 
 }
 
