3c35a6b0bbc781b9eed49794ad2a5836470cf867
Abhiram-BVS <abhiram.bachiraju@winfosolutions.com>
2022-02-16 19:39:01 +0530

    DH_27

--- src/main/java/com/winfo/scripts/HS2SeleniumKeyWords.java
+++ src/main/java/com/winfo/scripts/HS2SeleniumKeyWords.java
@@ -12692,9 +12692,10 @@ public class HS2SeleniumKeyWords implements SeleniumKeyWordsInterface {
 			log.error("Failed during  dropdownValues" + scripNumber);
 			System.out.println(e);
 		}
-		try {
-			if (param1.equalsIgnoreCase("FIN-7056-Generate Customer Statements")
-					&& (param2.equalsIgnoreCase("Legal Entity") || param2.equalsIgnoreCase("Customer Name"))) {
+		//DH 27
+				try {
+					if ((param1.equalsIgnoreCase("FIN-7056-Generate Customer Statements") || param1.equalsIgnoreCase("FIN-7077-Customer Statement"))
+							&& (param2.equalsIgnoreCase("Legal Entity") || param2.equalsIgnoreCase("Customer Name"))){
 				WebDriverWait wait = new WebDriverWait(driver, fetchConfigVO.getWait_time());
 				Thread.sleep(10000);
 				wait.until(ExpectedConditions.presenceOfElementLocated(
@@ -12856,15 +12857,16 @@ public class HS2SeleniumKeyWords implements SeleniumKeyWordsInterface {
 					enter(driver, fetchMetadataVO, fetchConfigVO);
 
 					Thread.sleep(10000);
-
+					//DH 27
 					WebElement select = driver.findElement(By
 
-							.xpath("//*[text()='Name']/following::span[normalize-space(text())='" + keysToSend + "']"));
+							.xpath("//*[text()='Name']/following::*[normalize-space(text())='" + keysToSend + "']"));
 
 //								clickValidateXpath(driver, fetchMetadataVO, select, fetchConfigVO); 
 
 					select.click();
-
+//					DH 27
+					Thread.sleep(1000);
 					WebElement searchok = driver
 
 							.findElement(By.xpath("//span[text()='Name']/following::a[text()='OK']"));
@@ -12913,10 +12915,10 @@ public class HS2SeleniumKeyWords implements SeleniumKeyWordsInterface {
 					enter(driver, fetchMetadataVO, fetchConfigVO);
 
 					Thread.sleep(6000);
-
+//					DH27
 					WebElement select = driver.findElement(By
 
-							.xpath("//*[text()='Name']/following::span[normalize-space(text())='" + keysToSend + "']"));
+							.xpath("//*[text()='Name']/following::*[normalize-space(text())='" + keysToSend + "']"));
 
 					// clickValidateXpath(driver, fetchMetadataVO, select, fetchConfigVO);
 
