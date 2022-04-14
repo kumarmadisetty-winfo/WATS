c9a5c052f900c7193327df4b0c356376488b1e2b
priya552 <priya.agarwal@winfosolutions.com>
2022-04-14 14:00:31 +0530

    downloading files with java nio
    downloading files with java nio

--- src/main/java/com/winfo/services/TestScriptExecService.java
+++ src/main/java/com/winfo/services/TestScriptExecService.java
@@ -109,6 +109,7 @@ import com.winfo.vo.PyJabScriptDto;
 import com.winfo.vo.UpdateScriptParamStatus;
 
 import org.apache.commons.io.FileUtils;
+import static java.nio.file.StandardOpenOption.CREATE_NEW;
 @Service
 public class TestScriptExecService {
 
@@ -627,8 +628,12 @@ public class TestScriptExecService {
 					.bucketName(bucketName).objectName(objectName).build());
 
 			String imageName = objectName.substring(objectName.lastIndexOf("/") + 1, objectName.length());
+			File file = new File(imagePath + imageName);
 			try (final InputStream stream = getResponse.getInputStream();
-					final OutputStream outputStream = new FileOutputStream(imagePath + imageName)) {
+				//	final OutputStream outputStream = new FileOutputStream(imagePath + imageName)
+					 
+					final OutputStream outputStream = Files.newOutputStream(file.toPath(),CREATE_NEW)		
+					) {
 				// use fileStream
 				byte[] buf = new byte[8192];
 				int bytesRead;
