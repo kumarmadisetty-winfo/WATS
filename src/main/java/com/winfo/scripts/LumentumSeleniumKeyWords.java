8ae9b00cf226fd9d37a4a3510de8afc36a7f6775
vijaykwinfo <82642450+vijaykwinfo@users.noreply.github.com>
2022-05-13 09:01:22 +0530

    Update LumentumSeleniumKeyWords.java

--- src/main/java/com/winfo/scripts/LumentumSeleniumKeyWords.java
+++ src/main/java/com/winfo/scripts/LumentumSeleniumKeyWords.java
@@ -18498,7 +18498,12 @@ public class LumentumSeleniumKeyWords implements SeleniumKeyWordsInterface {
 		
 	}
 
-
+@Override
+public void loginSSOApplication(WebDriver driver, FetchConfigVO fetchConfigVO, FetchMetadataVO fetchMetadataVO,
+			String type1, String type2, String type3, String param1, String param2, String param3, String keysToSend,
+			String value) throws Exception {
+		
+}
 
 
 }
