ed7ffea6af38c59ce1dcbdf2e94121817637320a
vijaykwinfo <82642450+vijaykwinfo@users.noreply.github.com>
2022-05-13 09:03:45 +0530

    Update WATS01SeleniumKeyWords.java

--- src/main/java/com/winfo/scripts/WATS01SeleniumKeyWords.java
+++ src/main/java/com/winfo/scripts/WATS01SeleniumKeyWords.java
@@ -17443,6 +17443,11 @@ public void oicMouseHover(WebDriver driver, String param1, String param2,
 		}
 		
 	}
-
+@Override
+public void loginSSOApplication(WebDriver driver, FetchConfigVO fetchConfigVO, FetchMetadataVO fetchMetadataVO,
+			String type1, String type2, String type3, String param1, String param2, String param3, String keysToSend,
+			String value) throws Exception {
+		
+}
 
 }
