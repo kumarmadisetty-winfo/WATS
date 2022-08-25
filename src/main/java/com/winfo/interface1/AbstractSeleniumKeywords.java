df5ca6ad4b2073a5bec6995f72963efaba80ee1a
SinghAbhij <79513716+SinghAbhij@users.noreply.github.com>
2022-08-25 12:52:25 +0530

    Update AbstractSeleniumKeywords.java

--- src/main/java/com/winfo/interface1/AbstractSeleniumKeywords.java
+++ src/main/java/com/winfo/interface1/AbstractSeleniumKeywords.java
@@ -260,6 +260,7 @@ public abstract class AbstractSeleniumKeywords {
 
 					String imageName = objectName.substring(objectName.lastIndexOf("/") + 1, objectName.length());
 					File file = new File(screenshotPath + FORWARD_SLASH + imageName);
+					logger.info(file.exists() + "FileExist or not ******" + file.getCanonicalPath());
 					if (!file.exists()) {
 						try (final InputStream stream = getResponse.getInputStream();
 								// final OutputStream outputStream = new FileOutputStream(imagePath + imageName)
