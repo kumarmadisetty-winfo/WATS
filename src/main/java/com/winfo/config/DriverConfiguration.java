b9faad6ed275f54622c989009241cbfbcad9161b
SinghAbhij <79513716+SinghAbhij@users.noreply.github.com>
2022-09-01 10:20:45 +0530

    new changes for supporting firefox and edge also
    -- new changes for supporting firefox and edge for run automation.

--- src/main/java/com/winfo/config/DriverConfiguration.java
+++ src/main/java/com/winfo/config/DriverConfiguration.java
@@ -8,11 +8,11 @@ import java.util.concurrent.TimeUnit;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.jfree.util.Log;
+import org.openqa.selenium.Platform;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.edge.EdgeOptions;
+import org.openqa.selenium.firefox.FirefoxOptions;
 import org.openqa.selenium.remote.CapabilityType;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
@@ -27,65 +27,72 @@ import com.winfo.services.FetchConfigVO;
 @Component
 @RefreshScope
 public class DriverConfiguration {
-	
+
 	public final Logger logger = LogManager.getLogger(DriverConfiguration.class);
 
 	@Value("${configvO.config_url}")
-	private String config_url;
+	private String configUrl;
 
-	@SuppressWarnings("deprecation")
 	public WebDriver getWebDriver(FetchConfigVO fetchConfigVO) throws MalformedURLException {
 		logger.info("Start of get web driver method");
 		WebDriver driver = null;
 		String os = System.getProperty("os.name").toLowerCase();
 		if (BrowserConstants.CHROME.value.equalsIgnoreCase(fetchConfigVO.getBrowser())) {
 			System.setProperty(DriverConstants.CHROME_DRIVER.value, fetchConfigVO.getChrome_driver_path());
-			System.setProperty("java.awt.headless", "false");
-			// System.setProperty(DriverConstants.CHROME_DRIVER.value,
-			// "C:\\Users\\watsadmin\\Documents\\Selenium Grid\\chromedriver.exe");
+			System.setProperty(BrowserConstants.AWT_HEADLESS.value, "false");
 			Map<String, Object> prefs = new HashMap<String, Object>();
-			prefs.put("profile.default_content_settings.popups", 0);
-			prefs.put("download.default_directory", fetchConfigVO.getDownlod_file_path());
+			prefs.put(BrowserConstants.PROFILE_DEFAULT_CONTENT_SETTING.value, 0);
+			prefs.put(BrowserConstants.DOWNLOAD_DEFAULT_DIRECTORY.value, fetchConfigVO.getDownlod_file_path());
 			ChromeOptions options = new ChromeOptions();
 			DesiredCapabilities cap = DesiredCapabilities.chrome();
 			if (os.contains("win")) {
-				System.out.println("windows location");
-				options.setBinary("C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe");// cap.setCapability("chrome.binary",
-																										// "C:\\Program
-																										// Files
-																										// (x86)\\Google\\Chrome\\Application\\chrome.exe");
+				logger.info("windows location");
+				options.setBinary("/Program Files/Google/Chrome/Application/chrome.exe");
 			} else {
-				System.out.println("linux location");
+				logger.info("linux location");
 				options.setBinary("/usr/bin/google-chrome");
 			}
-
-//			options.setBinary("C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe");
-//			options.setBinary("/usr/bin/google-chrome");
-//			options.addArguments("headless");
-//			options.addArguments("--no-sandbox");
-			options.addArguments("start-maximized");
-			options.addArguments("--no-sandbox");
+			options.addArguments(BrowserConstants.START_MAXIMIZED.value);
+			options.addArguments(BrowserConstants.NO_SENDBOX.value);
 //			options.addArguments("--headless");
-			options.addArguments("--enable-automation");
-			options.addArguments("test-type=browser");
-			options.addArguments("disable-infobars");
+			options.addArguments(BrowserConstants.ENABLE_AUTOMATION.value);
+			options.addArguments(BrowserConstants.TEST_TYPE.value);
+			options.addArguments(BrowserConstants.DISABLE_INFOBARS.value);
 			options.setExperimentalOption("prefs", prefs);
-
 			cap.setCapability(CapabilityType.ACCEPT_SSL_CERTS, true);
 			cap.setCapability(ChromeOptions.CAPABILITY, options);
 
 //			 driver = new ChromeDriver(cap);
-			driver = new RemoteWebDriver(new URL(config_url), cap);
+			driver = new RemoteWebDriver(new URL(configUrl), cap);
 //			http://watsudgs01.winfosolutions.com:4444/wd/hub
-
 		} else if (BrowserConstants.FIREFOX.value.equalsIgnoreCase(fetchConfigVO.getBrowser())) {
-			System.setProperty(DriverConstants.FIREFOX_DRIVER.value, fetchConfigVO.getFirefox_driver_path());
-			DesiredCapabilities capabilities = DesiredCapabilities.firefox();
-			capabilities.setCapability("marionette", true);
-			driver = new FirefoxDriver(capabilities);
-		} else if (BrowserConstants.IE.value.equalsIgnoreCase(fetchConfigVO.getBrowser())) {
-			System.setProperty(DriverConstants.IE_DRIVER.value, fetchConfigVO.getIe_driver_path());
-			driver = new InternetExplorerDriver();
+			System.setProperty(DriverConstants.FIREFOX_DRIVER.value,
+					"/Github/EBS-Automation-POC/Driver/geckodriver.exe");
+//			System.setProperty(DriverConstants.FIREFOX_DRIVER.value, fetchConfigVO.getFirefox_driver_path());
+
+			System.setProperty(BrowserConstants.AWT_HEADLESS.value, "false");
+			FirefoxOptions options = new FirefoxOptions();
+			if (!os.contains("win")) {
+				options.setBinary("/Program Files/Mozilla Firefox/firefox.exe");
+			} else {
+				logger.info("linux location");
+				options.setBinary("/usr/bin/firefox");
+			}
+//			options.addArguments("--headless");
+			options.addArguments(BrowserConstants.NO_SENDBOX.value);
+			options.addArguments(BrowserConstants.ENABLE_AUTOMATION.value);
+			options.addArguments(BrowserConstants.DISABLE_INFOBARS.value);
+			options.setCapability(BrowserConstants.MARIONETTE.value, true);
+			// driver = new FirefoxDriver(options);
+			driver = new RemoteWebDriver(new URL(configUrl), options);
+		} else if (BrowserConstants.EDGE.value.equalsIgnoreCase(fetchConfigVO.getBrowser())) {
+//			System.setProperty(DriverConstants.MS_EDGE.value, "/Github/EBS-Automation-POC/Driver/msedgedriver.exe");
+			System.setProperty(DriverConstants.MS_EDGE.value, fetchConfigVO.getEdge_driver_path());
+			DesiredCapabilities capability = DesiredCapabilities.edge();
+			capability.setBrowserName("MicrosoftEdge");
+			capability.setPlatform(Platform.LINUX);
+//			driver = new EdgeDriver();
+			driver = new RemoteWebDriver(new URL(configUrl), capability);
 		}
 		if (driver != null) {
 			logger.info("Browser launched...");
