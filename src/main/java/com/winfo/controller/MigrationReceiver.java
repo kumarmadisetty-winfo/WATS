901502863b89558e4ad82a2f4c13e8349ec61de5
SinghAbhij <79513716+SinghAbhij@users.noreply.github.com>
2022-09-13 19:15:33 +0530

    test run migration response related changes
    -- test run migration response related changes

--- src/main/java/com/winfo/controller/MigrationReceiver.java
+++ src/main/java/com/winfo/controller/MigrationReceiver.java
@@ -1,6 +1,8 @@
 package com.winfo.controller;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -11,9 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.winfo.services.CentralToCustomerPostService;
 import com.winfo.services.GetApiValidationMigrationService;
-import com.winfo.services.PostApiValidationMigrationService;
 import com.winfo.services.TestRunMigrationGetService;
-import com.winfo.vo.ApiValidationMigrationDto;
 import com.winfo.vo.DomGenericResponseBean;
 import com.winfo.vo.LookUpCodeVO;
 import com.winfo.vo.ResponseDto;
@@ -22,35 +22,35 @@ import com.winfo.vo.WatsMasterDataVOList;
 
 @RestController
 public class MigrationReceiver {
-	
+
 	public final Logger logger = LogManager.getLogger(MigrationReceiver.class);
-	
+
 	@Autowired
 	CentralToCustomerPostService service;
-	
+
 	@Autowired
 	GetApiValidationMigrationService apiValidationMigrationService;
-	
+
 	@Autowired
 	TestRunMigrationGetService testRunMigrateGetService;
-	
+
 	@PostMapping("/centralToCustomerScriptMigrate")
 	public List<DomGenericResponseBean> scriptMetaDataListFromCentral(@RequestBody WatsMasterDataVOList mastervolist) {
 		return service.saveScriptMasterDtls(mastervolist);
-		
+
 	}
-	
+
 	@PostMapping("/apiValidationMigrationReceiver")
 	public ResponseDto apiValidationMigration(@RequestBody List<LookUpCodeVO> lookUpCodeVOData) {
 		return apiValidationMigrationService.apiValidationMigration(lookUpCodeVOData);
 	}
-	
+
 	@PostMapping("/testRunMigrationToCustomer")
-	public List<DomGenericResponseBean> pluginData(@RequestBody List<TestRunMigrationDto> listOfTestRunDto) {
-		return testRunMigrateGetService.centralRepoData(listOfTestRunDto);
-
+	public Map<String, List<DomGenericResponseBean>> pluginData(
+			@RequestBody List<TestRunMigrationDto> listOfTestRunDto) {
+		Map<String, List<DomGenericResponseBean>> response = new HashMap<>();
+		response.put("response", testRunMigrateGetService.centralRepoData(listOfTestRunDto));
+		return response;
 	}
-	
+
 }
-
-
