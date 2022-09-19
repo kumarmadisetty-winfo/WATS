8415eb4b5abd01c49f7100cd32d66d3a02e97d79
Abhijit Singh <abhijit.singh@winfosolutions.com>
2022-09-19 09:55:39 +0000

    removed unused methods
    -- removed unused methods

--- src/main/java/com/winfo/utils/StringUtils.java
+++ src/main/java/com/winfo/utils/StringUtils.java
@@ -8,18 +8,6 @@ public class StringUtils {
 		return projectPath + fileName;
 	}
 
-	public static boolean isNotNullorBlank(String input) {
-		
-		return input.isEmpty();
-	}
-
-	public static final boolean isNullOrBlank(String input) {
-		return input == null || "".equals(input.trim()) || "null".equals(input.trim());
-	}
-
-	public static final boolean isNullOrBlank(Object o) {
-		return !(o != null && !"".equals(o.toString()));
-	}
 
 	public static String convertToString(Object input) {
 		String outputString = null;
@@ -29,20 +17,6 @@ public class StringUtils {
 		return outputString;
 	}
 
-	public static final int convertStringToInteger(final String input) {
-		return convertStringToInteger(input, 0);
-	}
-
-	public static final int convertStringToInteger(final String input, int defaultValue) {
-		return StringUtils.isNullOrBlank(input) ? defaultValue : Integer.parseInt(input);
-	}
-
-	public static String replaceString(String input, String oldChar, String newChar) {
-		if (isNotNullorBlank(input)) {
-			input = input.replace(oldChar, newChar);
-		}
-		return convertToString(input);
-	}
 	
 	private StringUtils() {
 		throw new IllegalStateException("Utility class");
