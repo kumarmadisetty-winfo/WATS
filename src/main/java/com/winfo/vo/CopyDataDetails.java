6f1081216bb880ba8639a03868e3e8e5bd2254d6
SinghAbhij <79513716+SinghAbhij@users.noreply.github.com>
2022-11-03 14:25:06 +0530

    Jira ticket no. WATS-1864
    https://winfosolutions.atlassian.net/browse/WATS-1864

--- src/main/java/com/winfo/vo/CopyDataDetails.java
+++ src/main/java/com/winfo/vo/CopyDataDetails.java
@@ -1,29 +1,27 @@
 package com.winfo.vo;
 
-
-
-import com.fasterxml.jackson.annotation.JsonProperty;
-
 public class CopyDataDetails {
 
-	@JsonProperty("product_version_new")
-	private String product_version_new;
+	private String productVersionNew;
 	
-	@JsonProperty("product_version_old")
-    private String product_version_old;
-	public String getProduct_version_new() {
-		return product_version_new;
-	}
-	public void setProduct_version_new(String product_version_new) {
-		this.product_version_new = product_version_new;
+    private String productVersionOld;
+
+	public String getProductVersionNew() {
+		return productVersionNew;
 	}
 
-	public String getProduct_version_old() {
-		return product_version_old;
+	public void setProductVersionNew(String productVersionNew) {
+		this.productVersionNew = productVersionNew;
 	}
-	public void setProduct_version_old(String product_version_old) {
-		this.product_version_old = product_version_old;
+
+	public String getProductVersionOld() {
+		return productVersionOld;
 	}
+
+	public void setProductVersionOld(String productVersionOld) {
+		this.productVersionOld = productVersionOld;
+	}
+
 	
 	
 	
