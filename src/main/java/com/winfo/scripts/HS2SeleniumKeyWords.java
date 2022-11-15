bde99cb5988d4d0de1ea32cb06d2688bf644490d
saikumar793 <96423752+saikumar793@users.noreply.github.com>
2022-11-15 15:00:28 +0530

    Api changes

--- src/main/java/com/winfo/scripts/HS2SeleniumKeyWords.java
+++ src/main/java/com/winfo/scripts/HS2SeleniumKeyWords.java
@@ -19094,21 +19094,6 @@ public class HS2SeleniumKeyWords extends AbstractSeleniumKeywords implements Sel
 	}
 
 	@Override
-	public void apiValidationResponse(ScriptDetailsDto fetchMetadataVO, Map<String, String> accessTokenStorage,
-			ApiValidationVO api, CustomerProjectDto customerDetails,FetchConfigVO fetchConfigVO) throws Exception {
-		// TODO Auto-generated method stub
-
-	}
-
-
-
-	@Override
-	public boolean validation(ScriptDetailsDto fetchMetadataVO, ApiValidationVO api) {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	@Override
 	public void navigateToBackPage(WebDriver driver, ScriptDetailsDto fetchMetadataVO, FetchConfigVO fetchConfigVO,
 			CustomerProjectDto customerDetails) {
 		// TODO Auto-generated method stub
