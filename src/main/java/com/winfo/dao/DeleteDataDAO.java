4361a7722a9f5e32f5ed7dfef31df0f547278bfe
SinghAbhij <79513716+SinghAbhij@users.noreply.github.com>
2022-12-06 18:49:20 +0530

    jira ticket no. WATS-1958
    https://winfosolutions.atlassian.net/browse/WATS-1958

--- src/main/java/com/winfo/dao/DeleteDataDAO.java
+++ src/main/java/com/winfo/dao/DeleteDataDAO.java
@@ -9,9 +9,11 @@ import org.hibernate.Session;
 import org.hibernate.Transaction;
 import org.hibernate.query.Query;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Repository;
 import org.springframework.web.bind.annotation.RequestBody;
 
+import com.winfo.model.ScriptMaster;
 import com.winfo.vo.DeleteScriptsData;
 import com.winfo.vo.DomGenericResponseBean;
 
@@ -20,6 +22,9 @@ public class DeleteDataDAO {
 
 	@Autowired
 	private EntityManager entityManager;
+	
+	@Autowired
+	ApplicationContext appContext;
 
 	public List<DomGenericResponseBean> deleteData(@RequestBody DeleteScriptsData deletescriptsdata) {
 		Session session = entityManager.unwrap(Session.class);
@@ -33,29 +38,42 @@ public class DeleteDataDAO {
 		List<Integer> scriptIds = deletescriptsdata.getScript_id();
 		int i = 0;
 		int count = 0;
-		try {
-			for (i = 0; i < scriptIds.size(); i++) {
-				count = 0;
-				Integer scriptId = scriptIds.get(i);
-				if (scriptIds.size() == 1) {
-					Query<?> query2 = session
-							.createQuery("select scriptNumber from ScriptMaster where scriptId=" + scriptId);
-					scriptNumber = (String) query2.getSingleResult();
 
-				}
-				Query<?> query = session.createQuery("delete from ScriptMaster where scriptId=" + scriptId);
-				Query<?> query1 = session.createQuery("delete from ScriptMetaData where scriptMaster.scriptId=" + scriptId);
+		if (deletescriptsdata.isDeleteAll()) {
 
-				deleted = query1.executeUpdate();
-				deleted1 = query.executeUpdate();
-
-				if (deleted == 0 && deleted1 == 0) {
-					count = 1;
-				}
-
+			List<ScriptMaster> listOfScriptmasterObj = appContext.getBean(this.getClass()).getScriptIdFromProductVersion(deletescriptsdata.getProd_ver());
+			for(ScriptMaster scriptMasterObj : listOfScriptmasterObj) {
+				scriptIds.add(scriptMasterObj.getScriptId());
+				session.delete(scriptMasterObj);
+				i++;
 			}
-		} catch (Exception e) {
-			e.printStackTrace();
+			
+		} else {
+			try {
+				for (i = 0; i < scriptIds.size(); i++) {
+					count = 0;
+					Integer scriptId = scriptIds.get(i);
+					if (scriptIds.size() == 1) {
+						Query<?> query2 = session
+								.createQuery("select scriptNumber from ScriptMaster where scriptId=" + scriptId);
+						scriptNumber = (String) query2.getSingleResult();
+
+					}
+					Query<?> query = session.createQuery("delete from ScriptMaster where scriptId=" + scriptId);
+					Query<?> query1 = session
+							.createQuery("delete from ScriptMetaData where scriptMaster.scriptId=" + scriptId);
+
+					deleted = query1.executeUpdate();
+					deleted1 = query.executeUpdate();
+
+					if (deleted == 0 && deleted1 == 0) {
+						count = 1;
+					}
+
+				}
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
 		}
 		DomGenericResponseBean response = new DomGenericResponseBean();
 		if (count == 1) {
@@ -76,5 +94,18 @@ public class DeleteDataDAO {
 
 		tx.commit();
 		return bean;
+	}
+	
+	
+	@SuppressWarnings("unchecked")
+	public List<ScriptMaster> getScriptIdFromProductVersion(String productVersion) {
+		
+		Session session = entityManager.unwrap(Session.class);
+		
+		Query<?> query = session.createQuery("from ScriptMaster where productVersion =:productVersion");
+		query.setParameter("productVersion", productVersion);
+		
+		return (List<ScriptMaster>)query.getResultList();
+		
 	}
 }
--- src/main/java/com/winfo/vo/DeleteScriptsData.java
+++ src/main/java/com/winfo/vo/DeleteScriptsData.java
@@ -9,11 +9,28 @@ public class DeleteScriptsData {
 	@JsonProperty("script_id")
 	private List<Integer> script_id= new ArrayList<Integer>();
 	
+	private String prod_ver;
+	
+	private boolean deleteAll;
+	
 	public List<Integer> getScript_id() {
 		return script_id;
 	}
 	public void setScript_id(List<Integer> script_id) {
 		this.script_id = script_id;
+	}
+	
+	public String getProd_ver() {
+		return prod_ver;
+	}
+	public void setProd_ver(String prod_ver) {
+		this.prod_ver = prod_ver;
+	}
+	public boolean isDeleteAll() {
+		return deleteAll;
+	}
+	public void setDeleteAll(boolean deleteAll) {
+		this.deleteAll = deleteAll;
 	}
 
 }
