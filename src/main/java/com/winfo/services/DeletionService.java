61b5283352f8ba2a5fd9631e0587e92e5a853e70
Uday Pratap Singh <uditsingh942594@gmail.com>
2023-01-17 12:33:27 +0530

    changes

--- src/main/java/com/winfo/services/DeletionService.java
+++ src/main/java/com/winfo/services/DeletionService.java
@@ -70,6 +70,9 @@ public class DeletionService{
 	@Autowired
 	TestScriptExecService testScriptExecService;
 	
+	@Autowired
+	ScriptDeletionService scriptDeletionService;
+	
 //	@Autowired
 //	AbstractSeleniumKeywords abstractSeleniumKeywords;
 
@@ -135,7 +138,8 @@ public class DeletionService{
 					try {
 						
 						deleteScriptDtlsForObjStoreAndSharePoint(testSetLineObj, customerDetails, provider, fetchConfigVO, deleteReportDtoObj.getTestSetId());
-						dataBaseEntry.getTestRunLinesDataByTestSetLineId(testSetLineObj);
+//						dataBaseEntry.getTestRunLinesDataByTestSetLineId(lineId);
+						scriptDeletionService.deleteScriptFromTestRun(Integer.parseInt(lineId));
 					} catch (Exception e) {
 						logger.error(e);
 						e.printStackTrace();
