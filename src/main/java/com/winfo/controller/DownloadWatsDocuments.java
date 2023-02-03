9bd96638f1f935c4bbed2afb46818892199c513b
prasadingole7 <prasad.ingole@winfosolutions.com>
2023-02-03 22:03:21 +0530

    changes

--- src/main/java/com/winfo/controller/DownloadWatsDocuments.java
+++ src/main/java/com/winfo/controller/DownloadWatsDocuments.java
@@ -1,10 +1,5 @@
 package com.winfo.controller;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.util.Optional;
 
 import javax.validation.Valid;
 
@@ -13,19 +8,13 @@ import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.mvc.method.annotation.StreamingResponseBody;
 
-import com.winfo.exception.WatsEBSCustomException;
 import com.winfo.services.WatsDocumentService;
-import com.winfo.services.WatsPluginService;
 import com.winfo.vo.WatsDocumentVo;
-import com.winfo.vo.WatsPluginMasterVO;
-import com.winfo.vo.WatsScriptAssistantVO;
 @RestController
 public class DownloadWatsDocuments {
 	
--- src/main/java/com/winfo/services/WatsDocumentService.java
+++ src/main/java/com/winfo/services/WatsDocumentService.java
@@ -1,61 +1,17 @@
 package com.winfo.services;
 
-import java.io.ByteArrayOutputStream;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.FileWriter;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.ObjectOutputStream;
-import java.io.OutputStream;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.zip.ZipEntry;
-import java.util.zip.ZipInputStream;
-import java.util.zip.ZipOutputStream;
-
-import javax.transaction.Transactional;
-
-import org.apache.commons.io.IOUtils;
-import org.apache.commons.lang3.StringUtils;
-import org.apache.logging.log4j.LogManager;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.core.io.InputStreamResource;
-import org.springframework.core.io.Resource;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.servlet.mvc.method.annotation.StreamingResponseBody;
-
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.google.common.io.ByteStreams;
-import com.oracle.bmc.ConfigFileReader;
-import com.oracle.bmc.auth.AuthenticationDetailsProvider;
-import com.oracle.bmc.auth.ConfigFileAuthenticationDetailsProvider;
-import com.oracle.bmc.model.BmcException;
-import com.oracle.bmc.objectstorage.ObjectStorageClient;
-import com.oracle.bmc.objectstorage.requests.GetObjectRequest;
-import com.oracle.bmc.objectstorage.responses.GetObjectResponse;
 import com.winfo.dao.WatsPluginDao;
-import com.winfo.exception.WatsEBSCustomException;
-import com.winfo.interface1.AbstractSeleniumKeywords;
-import com.winfo.model.ScriptMaster;
-import com.winfo.model.ScriptMetaData;
-import com.winfo.scripts.RunAutomation;
-import com.winfo.vo.DomGenericResponseBean;
+import com.winfo.utils.CommonObjectStoreUtils;
 import com.winfo.vo.WatsDocumentVo;
-import com.winfo.vo.WatsLoginVO;
-import com.winfo.vo.WatsPluginMasterVO;
-import com.winfo.vo.WatsPluginMetaDataVO;
-import com.winfo.vo.WatsScriptAssistantVO;
 
 @Service
 public class WatsDocumentService {
@@ -73,37 +29,12 @@ public class WatsDocumentService {
 	public static final String FORWARD_SLASH = "/";
 	@Autowired
 	WatsPluginDao dao;
+	@Autowired
+	CommonObjectStoreUtils commonObjectStoreUtils;
 	
 	public ResponseEntity<StreamingResponseBody> getWatsDocumentPDFFile(WatsDocumentVo documentVo) throws IOException {
 
-		GetObjectResponse response = null;
-		final ConfigFileReader.ConfigFile configFile = ConfigFileReader
-				.parse(new FileInputStream(new File(ociConfigPath)), ociConfigName);
-		final AuthenticationDetailsProvider provider = new ConfigFileAuthenticationDetailsProvider(configFile);
-		try (ObjectStorageClient client = new ObjectStorageClient(provider);) {
-				GetObjectRequest getObjectRequest = GetObjectRequest.builder().namespaceName(ociNamespace)
-						.bucketName(ociBucketName).objectName("watsDocuments/"+documentVo.getWatsVersion()+"/"+documentVo.getFileName()+".pdf").build();
-				response = client.getObject(getObjectRequest);
-				InputStream fis= response.getInputStream();
-				long len=response.getContentLength();
-		        byte[] targetArray = IOUtils.toByteArray(fis);
-				return ResponseEntity.ok().contentType(MediaType.APPLICATION_PDF).header("Content-Disposition", "attachment; filename=\""+documentVo.getFileName()+".pdf\"").header("Content-Length",""+response.getContentLength())
-						.body(out -> {
-							out.write(targetArray);
-							out.flush();
-							});
-		    } catch (WatsEBSCustomException e) {
-		    	log.error(e.getErrorMessage());
-				throw e;
-			} catch (BmcException e) {
-				log.error(documentVo.getFileName()+".pdf"+ " is not exist for "+documentVo.getWatsVersion()+" WATS Version");
-		    	throw new WatsEBSCustomException(e.getStatusCode(),documentVo.getFileName()+".pdf"+ " is not exist for "+documentVo.getWatsVersion()+" WATS Version",e);
-		    }catch (IOException e) {
-		    	log.error("Exception occured while returning file from service");
-				throw new WatsEBSCustomException(403, "Exception occured while returning file from service", e);
-			} catch (Exception e) {
-				log.error("Exception occured while downloading pdf from Object Store");
-		    	throw new WatsEBSCustomException(500, "Exception occured while downloading pdf from Object Store", e);
-		    }
+		return commonObjectStoreUtils.getFileFromCommonObjectStore("watsDocuments/"+documentVo.getWatsVersion()+"/",documentVo.getFileName(),MediaType.APPLICATION_PDF);
+		
 	}
 }
