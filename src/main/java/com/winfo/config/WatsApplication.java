d6b942b9a9e0f93d8e699529970f06e6e44842e3
Uday Pratap Singh <uditsingh942594@gmail.com>
2023-02-23 22:57:37 +0530

    changes

--- src/main/java/com/winfo/config/WatsApplication.java
+++ src/main/java/com/winfo/config/WatsApplication.java
@@ -4,7 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.scheduling.annotation.EnableAsync;
