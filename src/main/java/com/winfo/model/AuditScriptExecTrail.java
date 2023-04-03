e2f7c35f718d315213d0779fa37cd418302c5f37
saikumar793 <96423752+saikumar793@users.noreply.github.com>
2023-04-03 12:56:57 +0530

    Lombok builder changes

--- src/main/java/com/winfo/model/AuditScriptExecTrail.java
+++ src/main/java/com/winfo/model/AuditScriptExecTrail.java
@@ -9,6 +9,7 @@ import javax.persistence.Id;
 import javax.persistence.Table;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -17,6 +18,7 @@ import lombok.NoArgsConstructor;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Builder
 public class AuditScriptExecTrail implements Serializable {
 
 	private static final long serialVersionUID = 6915558297579903655L;
@@ -29,67 +31,5 @@ public class AuditScriptExecTrail implements Serializable {
 	private Date eventTime;
 	private String triggeredBy;
 	private String message;
-
-	public static class Builder {
-
-		private String correlationId;
-		private Integer testSetLineId;
-		private Integer stageId;
-		private Date eventTime;
-		private String triggeredBy;
-
-		public Builder() {
-		}
-
-		Builder(String correlationId, Integer testSetLineId, Integer stageId, Date eventTime, String triggeredBy) {
-			this.correlationId = correlationId;
-			this.testSetLineId = testSetLineId;
-			this.stageId = stageId;
-			this.eventTime = eventTime;
-			this.triggeredBy = triggeredBy;
-		}
-
-		public Builder correlationId(String correlationId) {
-			this.correlationId = correlationId;
-			return Builder.this;
-		}
-
-		public Builder testSetLineId(Integer testSetLineId) {
-			this.testSetLineId = testSetLineId;
-			return Builder.this;
-		}
-
-		public Builder stageId(Integer stageId) {
-			this.stageId = stageId;
-			return Builder.this;
-		}
-
-		public Builder eventTime(Date eventTime) {
-			this.eventTime = eventTime;
-			return Builder.this;
-		}
-
-		public Builder triggeredBy(String triggeredBy) {
-			this.triggeredBy = triggeredBy;
-			return Builder.this;
-		}
-
-		public AuditScriptExecTrail build() {
-
-			return new AuditScriptExecTrail(this);
-		}
-	}
-
-	private AuditScriptExecTrail(Builder builder) {
-		this.correlationId = builder.correlationId;
-		this.testSetLineId = builder.testSetLineId;
-		this.stageId = builder.stageId;
-		this.eventTime = builder.eventTime;
-		this.triggeredBy = builder.triggeredBy;
-	}
-
-	public static Builder builder() {
-		return new Builder();
-	}
 
 }
--- src/main/java/com/winfo/model/AuditStageLookup.java
+++ src/main/java/com/winfo/model/AuditStageLookup.java
@@ -5,6 +5,7 @@ import javax.persistence.Id;
 import javax.persistence.Table;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -13,44 +14,11 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
 public class AuditStageLookup {
 
 	@Id
 	private Integer stageId;
 	private String stageName;
-
-	public static class Builder {
-
-		private Integer stageId;
-		private String stageName;
-
-		public Builder() {
-		}
-
-		Builder(Integer stageId, String stageName) {
-			this.stageId = stageId;
-			this.stageName = stageName;
-		}
-
-		public Builder stageId(Integer stageId) {
-			this.stageId = stageId;
-			return Builder.this;
-		}
-
-		public Builder stageName(String stageName) {
-			this.stageName = stageName;
-			return Builder.this;
-		}
-
-		public AuditStageLookup build() {
-
-			return new AuditStageLookup(this);
-		}
-	}
-
-	private AuditStageLookup(Builder builder) {
-		this.stageId = builder.stageId;
-		this.stageName = builder.stageName;
-	}
 
 }
