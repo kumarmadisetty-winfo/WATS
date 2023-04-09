9155c806de0998e9e87c9c5a080ffa4dcc0ebe00
himasai239 <himasai.pentakoti@winfosolutions.com>
2023-04-10 01:17:12 +0530

    changes

--- src/main/java/com/winfo/config/DriverConfiguration.java
+++ src/main/java/com/winfo/config/DriverConfiguration.java
@@ -48,14 +48,13 @@ public class DriverConfiguration {
 		os = operatingSystem == null ? os : operatingSystem;
 		if (BrowserConstants.CHROME.getValue().equalsIgnoreCase(fetchConfigVO.getBrowser())) {
 			System.setProperty(DriverConstants.CHROME_DRIVER.getValue(), fetchConfigVO.getChrome_driver_path());
-			System.setProperty("headless", "false");
+//			System.setProperty("headless", "false");
 			Map<String, Object> prefs = new HashMap<>();
 			prefs.put(BrowserConstants.PROFILE_DEFAULT_CONTENT_SETTING.getValue(), 0);
 			ChromeOptions options = new ChromeOptions();
 			MutableCapabilities cap = new MutableCapabilities();
-//			cap.setCapability(CapabilityType.UNEXPECTED_ALERT_BEHAVIOUR, UnexpectedAlertBehaviour.IGNORE);
-			cap.setCapability(CapabilityType.UNHANDLED_PROMPT_BEHAVIOUR, UnexpectedAlertBehaviour.IGNORE);
-			if (os.contains("win")) {
+//			cap.setCapability(CapabilityType.UNHANDLED_PROMPT_BEHAVIOUR, UnexpectedAlertBehaviour.IGNORE);
+			if (!os.contains("win")) {
 				prefs.put(BrowserConstants.DOWNLOAD_DEFAULT_DIRECTORY.getValue(), fetchConfigVO.getDownlod_file_path());
 				options.setBinary("/Program Files/Google/Chrome/Application/chrome.exe");
 				cap.setCapability(CapabilityType.PLATFORM_NAME, Platform.WINDOWS);
@@ -75,11 +74,15 @@ public class DriverConfiguration {
 			options.addArguments(BrowserConstants.DISABLE_INFOBARS.getValue());
 			options.addArguments("--disable-popup-blocking");
 			options.addArguments("chrome.switches","--disable-extensions");
+//			options.addArguments("--acceptInsecureCerts");
 			options.setExperimentalOption("prefs", prefs);
-			cap.setCapability(ChromeOptions.CAPABILITY, options);
+			options.setUnhandledPromptBehaviour(UnexpectedAlertBehaviour.IGNORE);
 			options.setAcceptInsecureCerts(true);
+			options.setHeadless(false);
+			cap.setCapability(ChromeOptions.CAPABILITY, options);
 			try {
-				String url = "http://watsdev01.winfosolutions.com:4444";
+//				String url = "http://watsdev01.winfosolutions.com:4444";
+				String url = "http://localhost:4444";
 				driver = new RemoteWebDriver(new URL(url), cap);
 				logger.info("driver init success");
 			} catch (Exception e) {
