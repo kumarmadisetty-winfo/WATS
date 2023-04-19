43380b5c1729e4be54bf3dba634b1cc368483fd6
saikumar793 <96423752+saikumar793@users.noreply.github.com>
2023-04-19 17:15:58 +0530

    Changes

--- src/main/java/com/winfo/scripts/WOODSeleniumKeywords.java
+++ src/main/java/com/winfo/scripts/WOODSeleniumKeywords.java
@@ -3487,7 +3487,7 @@ public class WOODSeleniumKeywords extends AbstractSeleniumKeywords implements Se
 		}
 
 		// New code for PTP.PO.511
-		// DH 29
+		// DH 29 // HCM.ADM.022 (Submit)
 		try {
 			if (param1.equalsIgnoreCase("Submit")) {
 				WebDriverWait wait = new WebDriverWait(driver, fetchConfigVO.getWait_time());
@@ -3750,7 +3750,7 @@ public class WOODSeleniumKeywords extends AbstractSeleniumKeywords implements Se
 		}
 
 		// Action: Click Button
-
+		// HCM.ADM.022
 		try {
 
 			if (param2.equalsIgnoreCase("OK")) {
@@ -4836,7 +4836,8 @@ public class WOODSeleniumKeywords extends AbstractSeleniumKeywords implements Se
 				String lineNumber = fetchMetadataVO.getLineNumber();
 				service.saveXpathParams(scriptID, lineNumber, xpath);
 				return;
-			} else if (param1.equalsIgnoreCase("Close")) {
+			} //HCM.ADM.022
+			else if (param1.equalsIgnoreCase("Close")) {
 				WebDriverWait wait = new WebDriverWait(driver, fetchConfigVO.getWait_time());
 				wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath(("//button[text()=\"Cl\"]"))));
 				Thread.sleep(5000);
@@ -5422,7 +5423,7 @@ public class WOODSeleniumKeywords extends AbstractSeleniumKeywords implements Se
 			log.error("Failed during Republish clickButton" + scripNumber);
 			System.out.println(e);
 		}
-		//
+		//HCM.ADM.022 (Edit) (Review)
 		try {
 			// Changed == to equals method
 			if (param2.equals("")) {
@@ -5557,6 +5558,7 @@ public class WOODSeleniumKeywords extends AbstractSeleniumKeywords implements Se
 			String scripNumber = fetchMetadataVO.getScriptNumber();
 			log.error("Failed during clickButton" + scripNumber);
 		}
+		//HCM.ADM.022 (update)
 		try {
 			WebDriverWait wait = new WebDriverWait(driver, fetchConfigVO.getWait_time());
 			wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("(//*[normalize-space(text())=\"" + param1
@@ -6048,6 +6050,7 @@ public class WOODSeleniumKeywords extends AbstractSeleniumKeywords implements Se
 			log.error("Failed during List of Processes Meeting Search Criteria clickTableLink" + scripNumber);
 			System.out.println(e);
 		}
+			//HCM.ADM.022 (Search Results)
 		try {
 			// Changed == to equals method
 			if (param2.equals("")) {
@@ -8875,7 +8878,8 @@ public class WOODSeleniumKeywords extends AbstractSeleniumKeywords implements Se
 			System.out.println(e);
 		}
 
-		// DH 32
+		// DH 32 // HCM.ADM.022 (date)
+		
 		try {
 			WebDriverWait wait = new WebDriverWait(driver, fetchConfigVO.getWait_time());
 			wait.until(ExpectedConditions.presenceOfElementLocated(By
@@ -9712,7 +9716,7 @@ public class WOODSeleniumKeywords extends AbstractSeleniumKeywords implements Se
 			log.error("Failed during sendValue" + scripNumber);
 			System.out.println(e);
 		}
-		// HCM.ADM.111465 (Name)
+		// HCM.ADM.111465 (Name) HCM.ADM.024 (Person Number)
 		try {
 			WebDriverWait wait = new WebDriverWait(driver, fetchConfigVO.getWait_time());
 			wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("(//*[contains(text(),\"" + param1
@@ -12528,6 +12532,8 @@ public class WOODSeleniumKeywords extends AbstractSeleniumKeywords implements Se
 	public void dropdownValues(WebDriver driver, String param1, String param2, String param3, String keysToSend,
 			ScriptDetailsDto fetchMetadataVO, FetchConfigVO fetchConfigVO, CustomerProjectDto customerDetails)
 			throws Exception {
+		
+		//	HCM.ADM.022 (Action & Action Reason)
 		try {
 			if ((param1.equalsIgnoreCase("Adjust Balance") && param2.equalsIgnoreCase("Reason"))
 					|| (param1.equalsIgnoreCase("Update Employment") && param2.equalsIgnoreCase("Action")) || (param1.equalsIgnoreCase("Update Employment") && param2.equalsIgnoreCase("Action Reason"))) {
@@ -14861,7 +14867,8 @@ public class WOODSeleniumKeywords extends AbstractSeleniumKeywords implements Se
 			} catch (Exception ex) {
 				String scripNumber = fetchMetadataVO.getScriptNumber();
 				log.error("Failed during  dropdownValues" + scripNumber);
-
+				
+               //HCM.ADM.024 (Start time Am/PM)
 				try {
 					try {
 						WebElement popup1 = driver.findElement(By.xpath("//div[@class=\"AFDetectExpansion\"]"));
@@ -15104,15 +15111,17 @@ public class WOODSeleniumKeywords extends AbstractSeleniumKeywords implements Se
 			log.error("Failed during  dropdownValues" + scripNumber);
 			System.out.println(e);
 		}
+		
 		try {
 			WebDriverWait wait = new WebDriverWait(driver, fetchConfigVO.getWait_time());
 			wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath(
 					"//*[normalize-space(text())=\"Search\"]/following::*[normalize-space(text())=\"Name\"]/following::input[1]")));
 			WebElement searchResult = driver.findElement(By.xpath(
 					"//*[normalize-space(text())=\"Search\"]/following::*[normalize-space(text())=\"Name\"]/following::input[1]"));
+
 			typeIntoValidxpath(driver, keysToSend, searchResult, fetchConfigVO, fetchMetadataVO);
 			enter(driver, fetchMetadataVO, fetchConfigVO, customerDetails);
-			Thread.sleep(5000);
+		    Thread.sleep(5000);
 			WebElement text = driver.findElement(By.xpath("//span[normalize-space(text())=\"" + keysToSend + "\"]"));
 			text.click();
 			Thread.sleep(1000);
@@ -15134,6 +15143,7 @@ public class WOODSeleniumKeywords extends AbstractSeleniumKeywords implements Se
 			log.error("Failed during  dropdownValues" + scripNumber);
 			System.out.println(e);
 		}
+		
 		try {
 			WebElement button = driver.findElement(
 					By.xpath("//*[normalize-space(text())=\"Search\"]/following::*[normalize-space(text())=\"" + param2
