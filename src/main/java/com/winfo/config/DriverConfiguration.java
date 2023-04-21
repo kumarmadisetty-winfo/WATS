6de14f53fc26c5e10d3d6b59c1e77490a9c7b693
himasai239 <himasai.pentakoti@winfosolutions.com>
2023-04-21 14:30:11 +0530

    removed comments

--- src/main/java/com/winfo/config/DriverConfiguration.java
+++ src/main/java/com/winfo/config/DriverConfiguration.java
@@ -16,7 +16,6 @@ import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxOptions;
 import org.openqa.selenium.firefox.FirefoxProfile;
 import org.openqa.selenium.remote.CapabilityType;
-//import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
