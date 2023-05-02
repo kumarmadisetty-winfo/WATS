579749fdaf79401a13b7fbf3a04c2d531154c1dc
saikumar793 <saikumar.mattaparthi@winfosolutions.com>
2023-05-02 12:15:13 +0530

    changes

--- src/main/java/com/winfo/config/GraphqlSchemaReaderUtil.java
+++ src/main/java/com/winfo/config/GraphqlSchemaReaderUtil.java
@@ -1,9 +1,6 @@
 package com.winfo.config;
 
-import java.io.ByteArrayOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
-import java.nio.charset.StandardCharsets;
 
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.stereotype.Component;
@@ -13,17 +10,10 @@ import org.springframework.stereotype.Component;
 public class GraphqlSchemaReaderUtil {
 
 	public static String getSchemaFromFileName(final String filename) throws IOException {
-		InputStream inputStream = GraphqlSchemaReaderUtil.class.getResourceAsStream("/graphQl/" + filename + ".graphql");
-				 ByteArrayOutputStream result = new ByteArrayOutputStream();
-				byte[] buffer = new byte[1024];
-				int length;
-				while ((length = inputStream.read(buffer)) != -1) {
-				result.write(buffer, 0, length);
-				 }
-				String schema = result.toString(StandardCharsets.UTF_8.name());
-				return schema;
+		
 //		return new String(
 //				GraphqlSchemaReaderUtil.class.getResourceAsStream("/graphQl/" + filename + ".graphql").readAllBytes());
+		return "";
 	}
 
 }
