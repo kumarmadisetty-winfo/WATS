83b5e38bfbb8b3499389cf915d2ee80d3e6c5bb1
Pranayreddy Nimmala <pranayreddy.nimmala@winfosolutions.com>
2023-05-31 14:01:41 +0530

    keyvault integration

--- src/main/java/com/winfo/config/OCIDatabaseConfiguration.java
+++ src/main/java/com/winfo/config/OCIDatabaseConfiguration.java
@@ -14,8 +14,8 @@ import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
+import org.springframework.util.StringUtils;
 
-import com.oracle.bmc.Region;
 import com.oracle.bmc.auth.AuthenticationDetailsProvider;
 import com.oracle.bmc.auth.ConfigFileAuthenticationDetailsProvider;
 import com.oracle.bmc.identity.Identity;
@@ -26,7 +26,6 @@ import com.oracle.bmc.identity.responses.ListCompartmentsResponse;
 import com.oracle.bmc.keymanagement.KmsVaultClient;
 import com.oracle.bmc.keymanagement.model.VaultSummary;
 import com.oracle.bmc.keymanagement.requests.ListVaultsRequest;
-import com.oracle.bmc.keymanagement.responses.ListVaultsResponse;
 import com.oracle.bmc.secrets.SecretsClient;
 import com.oracle.bmc.secrets.model.Base64SecretBundleContentDetails;
 import com.oracle.bmc.secrets.requests.GetSecretBundleByNameRequest;
@@ -35,7 +34,7 @@ import com.oracle.bmc.secrets.responses.GetSecretBundleByNameResponse;
 @Configuration
 @ConfigurationProperties(prefix = "spring.datasource")
 public class OCIDatabaseConfiguration {
-	
+
 	public final Logger log = LogManager.getLogger(OCIDatabaseConfiguration.class);
 
 	@Value("${oci.config.path}")
@@ -52,69 +51,93 @@ public class OCIDatabaseConfiguration {
 
 	@Bean
 	@Primary
-	public DataSource getDataSource() throws IOException {
+	public DataSource getDataSource() throws Exception {
 		final String vaultId = getVaultId();
 		return DataSourceBuilder.create().url(getOracleUrl("Hostname", "SID", "Port", vaultId))
 				.username(getSecretFromVault("db_user", vaultId)).password(getSecretFromVault("db_password", vaultId))
 				.build();
 	}
 
-	private String getOracleUrl(String hostNameKey, String sidKey, String portKey, String vaultId) throws IOException {
-		String url = "jdbc:oracle:thin:@" + getSecretFromVault(hostNameKey, vaultId) + ":"
-				+ getSecretFromVault(portKey, vaultId) + ":" + getSecretFromVault(sidKey, vaultId);
-		return url;
+	private String getOracleUrl(String hostNameKey, String sidKey, String portKey, String vaultId) throws Exception {
+		StringBuilder url = (new StringBuilder()).append("jdbc:oracle:thin:@")
+				.append(getSecretFromVault(hostNameKey, vaultId)).append(":")
+				.append(getSecretFromVault(portKey, vaultId)).append(":").append(getSecretFromVault(sidKey, vaultId));
+		return url.toString();
 	}
 
-	private String getSecretFromVault(String secretName, String vaultId) throws IOException {
+	private String getSecretFromVault(String secretName, String vaultId) throws Exception {
 		GetSecretBundleByNameResponse getSecretBundleByNameResponse;
 		SecretsClient secretsClient = null;
+		byte[] secretValueDecoded = null;
 		try {
 			secretsClient = SecretsClient.builder()
 					.build(new ConfigFileAuthenticationDetailsProvider(ociConfigPath, ociConfigName));
+			getSecretBundleByNameResponse = secretsClient.getSecretBundleByName(
+					GetSecretBundleByNameRequest.builder().secretName(secretName).vaultId(vaultId).build());
+			Base64SecretBundleContentDetails base64SecretBundleContentDetails = (Base64SecretBundleContentDetails) getSecretBundleByNameResponse
+					.getSecretBundle().getSecretBundleContent();
+			secretValueDecoded = Base64.decodeBase64(base64SecretBundleContentDetails.getContent());
 		} catch (IOException e) {
-			log.error("Authentication failed for keyvault");
+			log.error("Authentication failed for keyvault {}", e.getMessage());
+			throw e;
+		} catch (Exception e) {
+			log.error("Failed while fetching the value {}", e.getMessage());
 			throw e;
 		}
-		getSecretBundleByNameResponse = secretsClient.getSecretBundleByName(
-				GetSecretBundleByNameRequest.builder().secretName(secretName).vaultId(vaultId).build());
-		Base64SecretBundleContentDetails base64SecretBundleContentDetails = (Base64SecretBundleContentDetails) getSecretBundleByNameResponse
-				.getSecretBundle().getSecretBundleContent();
-		byte[] secretValueDecoded = Base64.decodeBase64(base64SecretBundleContentDetails.getContent());
-		return new String(secretValueDecoded).replace("\n", "");
+		if (secretValueDecoded != null)
+			return new String(secretValueDecoded).replace("\n", "");
+		else {
+			log.error("Value is not present in vault with secret name {}",secretName);
+			throw new Exception(String.format("Value is not present in the vault with secret name %s", secretName));
+		}
 	}
 
-	private String getVaultId() throws IOException {
+	private String getVaultId() throws Exception {
+		Optional<VaultSummary> vaultSummary = null;
 		AuthenticationDetailsProvider provider = null;
 		try {
 			provider = new ConfigFileAuthenticationDetailsProvider(ociConfigPath, ociConfigName);
+			Identity identityClient = IdentityClient.builder().build(provider);
+			Compartment compartment = getCompartment(identityClient, provider);
+			vaultSummary = KmsVaultClient.builder().build(provider)
+					.listVaults(ListVaultsRequest.builder().compartmentId(compartment.getId()).build()).getItems()
+					.parallelStream().filter(vault -> vaultName.equalsIgnoreCase(vault.getDisplayName())).findFirst();
+
+			if (vaultSummary.isPresent())
+				return vaultSummary.get().getId();
+			else {
+				log.error("Vault is not present in the compartment ::" + compartment.getName());
+				throw new Exception(
+						String.format("Vault is not present in the compartment %s ", compartment.getName()));
+			}
 		} catch (IOException e) {
-			log.error("Authentication failed for keyvault");
+			log.error("Authentication failed for keyvault {}", e.getMessage());
+			throw e;
+		} catch (Exception e) {
+			log.error("Not able to get vault id {}", e.getMessage());
 			throw e;
 		}
-		Identity identityClient = IdentityClient.builder().build(provider);
-		Compartment compartment = getCompartment(identityClient, provider);
-		KmsVaultClient kmsValueClient = KmsVaultClient.builder().build(provider);
-		ListVaultsRequest listRequest = ListVaultsRequest.builder().compartmentId(compartment.getId()).build();
-		ListVaultsResponse listResponse = kmsValueClient.listVaults(listRequest);
-		Optional<VaultSummary> vaultSummary = listResponse.getItems().stream().filter(vault -> vaultName.equalsIgnoreCase(vault.getDisplayName()))
-				.findFirst();
-		
-		return vaultSummary.get().getId();
 	}
 
-	private Compartment getCompartment(Identity identityClient, AuthenticationDetailsProvider provider) {
-		Optional<Compartment> compartment = null;
+	private Compartment getCompartment(Identity identityClient, AuthenticationDetailsProvider provider)
+			throws Exception {
+		Optional<Compartment> compartment = Optional.empty();
 		String nextPageToken = null;
 		ListCompartmentsResponse response = null;
 		do {
-			response = identityClient.listCompartments(ListCompartmentsRequest.builder().limit(5)
+			response = identityClient.listCompartments(ListCompartmentsRequest.builder().limit(10)
 					.compartmentId(provider.getTenantId()).page(nextPageToken).build());
 
-			compartment = response.getItems().stream().filter(compart -> compartmentName.equalsIgnoreCase(compart.getName()))
-					.findFirst();
+			compartment = response.getItems().parallelStream()
+					.filter(compart -> compartmentName.equalsIgnoreCase(compart.getName())).findFirst();
 			nextPageToken = response.getOpcNextPage();
-		} while (compartment == null && nextPageToken != null);
-		return compartment.get();
+		} while (compartment.isEmpty() && StringUtils.hasLength(nextPageToken));
+		if (compartment.isPresent())
+			return compartment.get();
+		else {
+			log.error("Compartment is not present in OCI keyvault");
+			throw new Exception("Compartment is not present in OCI keyvault");
+		}
 	}
 
 }
