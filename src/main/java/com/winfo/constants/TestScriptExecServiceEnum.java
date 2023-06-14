83a273a858ab2470ae29a30f0fbc7e5f68e0db6b
himasai01 <104968925+himasai01@users.noreply.github.com>
2023-06-14 14:58:53 +0530

    changes

---
+++ src/main/java/com/winfo/constants/TestScriptExecServiceEnum.java
@@ -0,0 +1,17 @@
+package com.winfo.constants;
+
+public class TestScriptExecServiceEnum {
+
+    public static final String BACK_SLASH = "\\\\";
+    public static final String topic = "test-script-run";
+    public static final String PY_EXTN = ".py";
+    public static final String JPG_EXTENSION = ".jpg";
+    public static final String PASSED = "Passed";
+    public static final String FAILED = "Failed";
+    public static final String FAIL = "Fail";
+    public static final String PDF_EXTENSION = ".pdf";
+    public static final String SCREENSHOT = "Screenshot";
+    public static final String FORWARD_SLASH = "/";
+    public static final String PNG_EXTENSION = ".png";
+    
+}
