7f891d3947857649539c9f3939bcd9ad50fe3b56
prasadingole7 <prasad.ingole@winfosolutions.com>
2023-06-19 16:44:21 +0530

    changes

--- src/main/java/com/winfo/controller/TemplateDownloadController.java
+++ src/main/java/com/winfo/controller/TemplateDownloadController.java
@@ -34,7 +34,7 @@ public class TemplateDownloadController {
 	@ApiResponses(value = { @ApiResponse(code = 200, message = "Success"),
 			@ApiResponse(code = 404, message = "Not Found"), @ApiResponse(code = 400, message = "Bad Request Body"),
 			@ApiResponse(code = 500, message = "Internal Server Error") })
-	public ResponseEntity<StreamingResponseBody> generateTemplete(@PathVariable Optional<Integer> scriptId,@Valid @RequestBody String userName)
+	public ResponseEntity<StreamingResponseBody> generateTemplete(@PathVariable Optional<Integer> scriptId,@RequestBody String userName)
 			throws Exception {
 		StreamingResponseBody stream = out -> {
 			templateDownloaderService.generateTemplate(scriptId,userName).write(out);
