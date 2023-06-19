9169d489cfe57d809fef41fd2b44927acc7d36a1
prasadingole7 <prasad.ingole@winfosolutions.com>
2023-06-19 16:32:06 +0530

    changes

--- src/main/java/com/winfo/repository/CustomerRepository.java
+++ src/main/java/com/winfo/repository/CustomerRepository.java
@@ -16,7 +16,7 @@ public interface CustomerRepository extends JpaRepository<Customer, Integer>{
 	List<String> findListOfCustomers();
 	
 	@Query("select c.customerName from Customer c, UserRole r, User u where upper(u.userId)=upper(?1) and "
-			+ "upper(r.userId)=upper(u.userId) and (upper(r.userType)='SUPER_ADMIN' or c.customerId=u.customerId)")
+			+ "upper(r.userId)=upper(u.userId) and (upper(r.userType) in ('SUPER_ADMIN','SUPPORT') or c.customerId=u.customerId)")
 	List<String> findListOfCustomers(String userName);
 	
 //	@Query("SELECT customerName from Customer where customerId=?1")
