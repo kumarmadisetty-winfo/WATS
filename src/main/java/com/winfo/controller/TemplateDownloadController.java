ea1e6171dcf3e9ed331f33f91b200628174bca46
prasadingole7 <prasad.ingole@winfosolutions.com>
2023-06-20 09:29:02 +0530

    changes

--- src/main/java/com/winfo/controller/TemplateDownloadController.java
+++ src/main/java/com/winfo/controller/TemplateDownloadController.java
@@ -2,22 +2,14 @@ package com.winfo.controller;
 
 import java.util.Date;
 import java.util.Optional;
-
-import javax.validation.Valid;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.mvc.method.annotation.StreamingResponseBody;
-
 import com.winfo.serviceImpl.TemplateDownloadService;
-import com.winfo.vo.VersionHistoryDto;
-
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
@@ -29,12 +21,12 @@ public class TemplateDownloadController {
 	private TemplateDownloadService templateDownloaderService;
 
 	@ResponseBody
-	@PostMapping(value = { "/downloadTemplate", "/downloadTemplate/{scriptId}" })
+	@GetMapping(value = { "/downloadTemplate/{userName}", "/downloadTemplate/{userName}/{scriptId}" })
 	@ApiOperation(value = "Download Template ", notes = " Download Template ")
 	@ApiResponses(value = { @ApiResponse(code = 200, message = "Success"),
 			@ApiResponse(code = 404, message = "Not Found"), @ApiResponse(code = 400, message = "Bad Request Body"),
 			@ApiResponse(code = 500, message = "Internal Server Error") })
-	public ResponseEntity<StreamingResponseBody> generateTemplete(@PathVariable Optional<Integer> scriptId,@RequestBody String userName)
+	public ResponseEntity<StreamingResponseBody> generateTemplete(@PathVariable Optional<Integer> scriptId,@PathVariable String userName)
 			throws Exception {
 		StreamingResponseBody stream = out -> {
 			templateDownloaderService.generateTemplate(scriptId,userName).write(out);
