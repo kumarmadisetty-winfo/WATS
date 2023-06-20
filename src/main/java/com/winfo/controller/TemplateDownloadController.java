15f603bd7f0a4269750213449c9038a67897ed85
prasadingole7 <prasad.ingole@winfosolutions.com>
2023-06-20 09:36:24 +0530

    changes

--- src/main/java/com/winfo/controller/TemplateDownloadController.java
+++ src/main/java/com/winfo/controller/TemplateDownloadController.java
@@ -6,7 +6,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.mvc.method.annotation.StreamingResponseBody;
 import com.winfo.serviceImpl.TemplateDownloadService;
@@ -20,7 +19,6 @@ public class TemplateDownloadController {
 	@Autowired
 	private TemplateDownloadService templateDownloaderService;
 
-	@ResponseBody
 	@GetMapping(value = { "/downloadTemplate/{userName}", "/downloadTemplate/{userName}/{scriptId}" })
 	@ApiOperation(value = "Download Template ", notes = " Download Template ")
 	@ApiResponses(value = { @ApiResponse(code = 200, message = "Success"),
