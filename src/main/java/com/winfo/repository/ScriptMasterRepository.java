2c70fd4b489539bb7a18a751b09cccc37a48e492
prasadingole7 <prasad.ingole@winfosolutions.com>
2023-06-23 10:35:14 +0530

    changes

--- src/main/java/com/winfo/repository/ScriptMasterRepository.java
+++ src/main/java/com/winfo/repository/ScriptMasterRepository.java
@@ -1,5 +1,7 @@
 package com.winfo.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +9,7 @@ import com.winfo.model.ScriptMaster;
 
 @Repository
 public interface ScriptMasterRepository extends JpaRepository<ScriptMaster, Integer> {
-
+	
+	List<ScriptMaster> findByProductVersionAndModule(String productVersion, String module);
+	
 }
