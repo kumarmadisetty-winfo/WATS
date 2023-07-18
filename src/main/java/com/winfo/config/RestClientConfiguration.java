11b0e4f332f99712a46f1d92dd3f571eab252c05
Uday Pratap Singh <86412941+usingh-winfo@users.noreply.github.com>
2023-07-18 14:55:51 +0530

    Changes

--- src/main/java/com/winfo/config/RestClientConfiguration.java
+++ src/main/java/com/winfo/config/RestClientConfiguration.java
@@ -3,6 +3,7 @@ package com.winfo.config;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.reactive.function.client.WebClient;
 
@@ -26,6 +27,7 @@ public class RestClientConfiguration {
     @Bean
     public RestTemplate restTemplate() {
         return new RestTemplateBuilder()
+        		.messageConverters(new MappingJackson2HttpMessageConverter())
                 .build();
     }
 }
