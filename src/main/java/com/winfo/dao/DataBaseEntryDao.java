f33dc1ab1a3f19deb81705bf909f82dafa31d39a
Uday Pratap Singh <86412941+usingh-winfo@users.noreply.github.com>
2023-07-28 00:32:23 +0530

    Revert "Changes"
    This reverts commit ea0756405ca09757ec6945b90511d4befdf5aa5a.

--- src/main/java/com/winfo/dao/DataBaseEntryDao.java
+++ src/main/java/com/winfo/dao/DataBaseEntryDao.java
@@ -835,10 +835,10 @@ public class DataBaseEntryDao {
 				scriptDetailsDto.setIssueKey(
 						NULL_STRING.equals(String.valueOf(obj[21])) ? null : String.valueOf(obj[21]));
 				
-				scriptDetailsDto.setUniqueMandatory(
+				scriptDetailsDto.setOracleReleaseYear(
 						NULL_STRING.equals(String.valueOf(obj[22])) ? null : String.valueOf(obj[22]));
 				
-				scriptDetailsDto.setOracleReleaseYear(
+				scriptDetailsDto.setUniqueMandatory(
 						NULL_STRING.equals(String.valueOf(obj[23])) ? null : String.valueOf(obj[23]));
 				
 				listOfTestRunExecutionVo.add(scriptDetailsDto);
