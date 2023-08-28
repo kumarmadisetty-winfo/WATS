113f367eacc59821692a7e59d94403515ecbf0e1
saikumar793 <saikumar.mattaparthi@winfosolutions.com>
2023-08-28 14:51:35 +0530

    interface class

---
+++ src/main/java/com/winfo/service/UpdateTestSetRecords.java
@@ -0,0 +1,13 @@
+package com.winfo.service;
+
+import org.jvnet.hk2.annotations.Service;
+
+@Service
+public class UpdateTestSetRecords {
+	
+	public void updateTetSet(String testSetId,String LastUpdatedBy) {
+		
+		
+	}
+
+}
