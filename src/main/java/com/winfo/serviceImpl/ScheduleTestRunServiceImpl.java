5ca073276ce4a93c06d38c3ae5619f16cb5906d3
pingole-winfo <prasad.ingole@winfosolutions.com>
2023-09-01 13:53:00 +0530

    changes

--- src/main/java/com/winfo/serviceImpl/ScheduleTestRunServiceImpl.java
+++ src/main/java/com/winfo/serviceImpl/ScheduleTestRunServiceImpl.java
@@ -111,6 +111,8 @@ public class ScheduleTestRunServiceImpl implements ScheduleTestRunService {
 						scheduleSubJobVO.setStartDate(testRunVO.getStartDate());
 						scheduleSubJobVO.setUserName(scheduleJobVO.getSchedulerEmail());
 						scheduleSubJobVO.setEmail(testRunVO.getNotification());
+						scheduleSubJobVO.setTestRunName(subScheduleJob.getComments());
+						scheduleSubJobVO.setTestSetId(testSetRepository.findByTestRunName(subScheduleJob.getComments()).getTestRunId());
 						try {
 							WebClient webClient = WebClient.create(basePath + "/WATSservice/editScheduleTestRun");
 							Mono<String> result = webClient.post().syncBody(scheduleSubJobVO).retrieve()
@@ -124,7 +126,11 @@ public class ScheduleTestRunServiceImpl implements ScheduleTestRunService {
 					}
 				});
 			});
-			return new ResponseDto(HttpStatus.OK.value(), Constants.SUCCESS, scheduler.getJobId()+":Successfully updated the "+scheduler.getJobName()+" job");
+		 	scheduler.setStatus(Constants.YET_TO_START);
+		 	scheduler.setUpdatedBy(scheduleJobVO.getSchedulerEmail());
+		 	scheduler.setUpdatedDate(new Date());
+		 	schedulerRepository.save(scheduler);
+		 	return new ResponseDto(HttpStatus.OK.value(), Constants.SUCCESS, scheduler.getJobId()+":Successfully updated the "+scheduler.getJobName()+" job");
 		} catch (Exception e) {
 			logger.error(e.getMessage());
 			return new ResponseDto(HttpStatus.INTERNAL_SERVER_ERROR.value(), Constants.ERROR, e.getMessage());
